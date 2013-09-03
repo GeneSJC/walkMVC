1e788fa3861337fa54e807ee5ae769740d336f40
gene-sbay <geneb.contact@gmail.com>
2013-09-02 22:35:52 -0700

    since list of includes is growing, moved it into sep' file

--- webapp/walk_auth/access.php
+++ webapp/walk_auth/access.php
@@ -1,27 +1,6 @@
 <?php
 
-require_once '../../php/codeguy-Slim/Slim/Slim.php';
-
-require_once './load_smarty.php';
-require_once './load_session.php';	
-
-require_once '../../php/phpDataMapper/Base.php';
-require_once '../../php/phpDataMapper/Adapter/Mysql.php';
-
-require_once '../../php/form_config_base.php';
-
-
-	// START model includes - after base includes
-	
-require_once './post/post_mapper.php';
-require_once './post/post_ctrl.php';
-require_once './post/post_formcfg.php';
-
-require_once './user/user_mapper.php';
-require_once './user/user_ctrl.php';
-require_once './user/formcfg_login.php';
-
-	// END model includes
+require_once './app_includes.php';	
 
 
 
@@ -29,6 +8,7 @@ require_once './user/formcfg_login.php';
 
 $app = new \Slim\Slim();
 
+	// IMPORTANT: These includes must come after the $app declaration above & before the ->run() call below
 require_once './user/user_rest.php';
 require_once './post/post_rest.php';
 
---
+++ webapp/walk_auth/app_includes.php
@@ -0,0 +1,38 @@
+<?php
+
+	// 3rd-PARTY INCLUDES
+	
+require_once '../../php/codeguy-Slim/Slim/Slim.php';
+
+require_once '../../php/phpDataMapper/Base.php';
+require_once '../../php/phpDataMapper/Adapter/Mysql.php';
+
+
+
+
+	// walkMVC INCLUDES
+	
+require_once '../../php/form_config_base.php';
+
+
+
+	// app-specific INCLUDES
+
+require_once './load_smarty.php';
+require_once './load_session.php';	
+
+
+
+
+	// START model includes - after base includes
+	
+require_once './post/post_mapper.php';
+require_once './post/post_ctrl.php';
+require_once './post/post_formcfg.php';
+
+require_once './user/user_mapper.php';
+require_once './user/user_ctrl.php';
+require_once './user/formcfg_login.php';
+
+	// END model includes
+
