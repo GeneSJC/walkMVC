0bc1870cb371c0acef1a3e64ada6241d9931c14a
gene-sbay <geneb.contact@gmail.com>
2013-09-06 15:28:05 -0700

    refactoring for updated naming convention: rest_XYZ.php

--- webapp/walk_auth/access.php
+++ webapp/walk_auth/access.php
@@ -9,7 +9,7 @@ require_once './app_includes.php';
 $app = new \Slim\Slim();
 
 	// IMPORTANT: These includes must come after the $app declaration above & before the ->run() call below
-require_once './user/user_rest.php';
+require_once './user/rest_user.php';
 require_once './user/rest_recover.php';
 require_once './post/post_rest.php';
 
