4518775a7af6a9b8fd70db54e86583eefa85b91c
gene-sbay <geneb.contact@gmail.com>
2013-09-08 00:16:42 -0700

    added recover table to create

--- webapp/walk_auth/_test/db_create.php
+++ webapp/walk_auth/_test/db_create.php
@@ -6,14 +6,17 @@ require_once '../../../php/phpDataMapper/Adapter/Mysql.php';
 require_once '../util_db.php';
 
 require_once '../user/map_user.php';
+require_once '../user/map_recover.php';
 require_once '../post/map_post.php';
 
 $adapter = getDbAdapter();
 $userMapper = new UserMapper($adapter);
+$recoverMapper = new RecoverMapper($adapter);
 $postMapper = new PostMapper($adapter);
 echo  " <br/>\n";
 
 $userMapper->migrate(); 
+$recoverMapper->migrate(); 
 $postMapper->migrate(); 
 
 echo "Script finished without any known error";
