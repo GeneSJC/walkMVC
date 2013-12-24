f2b33f5599561a8445417c8fe772d7bf985b326e
gene-sbay <geneb.contact@gmail.com>
2013-12-24 11:59:43 -0800

    fixed APP_REST_ROOT bug

--- app_authDemo/user/rest_user.php
+++ app_authDemo/user/rest_user.php
@@ -48,7 +48,7 @@ function setFacebookLoginButtonData()
 	
 	$facebook_api = FacebookApiUtil::getFacebookApi();
 	
-	$smarty->assign("loginUrl", APP_WEB_ROOT . '/public/fbLogin');
+	$smarty->assign("loginUrl", APP_REST_ROOT . '/public/fbLogin');
 	$smarty->assign("fbAppId", $facebook_api->getAppID());
 }
 
