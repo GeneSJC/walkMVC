71f267f07a5be575de006058aab6f40da411701b
gene-sbay <geneb.contact@gmail.com>
2014-07-21 12:56:12 -0700

    added error reporting to demos

--- app_authDemo/access.php
+++ app_authDemo/access.php
@@ -1,5 +1,9 @@
 <?php
 
+// Comment out *error_reporting* when going to production 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 require_once './_util/app_cfg.php';
 require_once './_util/app_includes.php';
 
