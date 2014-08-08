c1397125eb3a09809dbd9509a34ee03c952d03b7
gene-sbay <geneb.contact@gmail.com>
2014-08-08 14:06:22 -0700

    added simple handy 404 catch-all redirect

--- app_simpleDemo/access.php
+++ app_simpleDemo/access.php
@@ -9,13 +9,34 @@ require_once './_util/app_includes.php';
 
 localInit();
 
-
 // =====================
 // ----- REST DEMO ------
 
 $app->get('/access/sayHello', 'sayHello' );
 function sayHello ()
 { echo "hello2"; }
+
+// =====================
+// ----- 404 Handler ------
+
+$app->notFound(function ()
+{
+	global $app;
+
+	BaseAppUtil::xlog('handling notFound');
+
+	$restStart = APP_REST_ROOT . '/post/home';
+	BaseAppUtil::xlog("Attempting to redirect to: $restStart");
+
+	$app->redirect($restStart);
+
+	return true;
+});
+
+
+
+// =====================
+// ----- Now run $app ------
 
 $app->run();
 
